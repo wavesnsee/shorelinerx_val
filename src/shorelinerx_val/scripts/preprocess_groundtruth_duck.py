e5e3a34772d110a06c3285e4ec573108a600f3c1
Florent <florent.fournier@wavesnsee.com>
2026-08-19 17:47:18 +0200

    debub preprocessing of groundtruth duck

--- src/shorelinerx_val/scripts/preprocess_groundtruth_duck.py
+++ src/shorelinerx_val/scripts/preprocess_groundtruth_duck.py
@@ -1,58 +1,80 @@
-from pathlib import Path
+import os
 import numpy as np
 import pandas as pd
 from netCDF4 import Dataset
-from scipy.interpolate import UnivariateSpline
-import matplotlib.pyplot as plt
+from pathlib import Path
 from datetime import datetime, timedelta
 
-
-
+# output file
 f_parquet = '/home/florent/Projects/Shoreliner_CNES/validation/groundtruth/beach_profiles_duck.parquet'
 
-# selection of transects used for validation
-transects = [-91, 1, 1006, 1097]
+fp_raw = os.path.join('/home/florent/dev/SDS_Benchmark/datasets/DUCK/', 'raw')
+filenames = sorted(Path('/home/florent/dev/SDS_Benchmark/datasets/DUCK/raw/').glob('*.nc'))
 
-# read raw profiles
-dir_raw = Path('/home/florent/dev/SDS_Benchmark/datasets/DUCK/raw/')
-ls_raw = sorted(dir_raw.glob('*.nc'))
+profiles = [-91, 1, 1006, 1097]
+pf_names = [str(_) for _ in profiles]
 
 # reference time
 ref_t = datetime(1970, 1, 1)
 
+# read all files and merge them
+survey_data = dict([])
+
+for fn in sorted(filenames):
+    data = Dataset(os.path.join(fp_raw,fn))
+    date_str = fn.name[-11:].split('.nc')[0]
+    if date_str == '19890523': continue     # skip problematic survey
+
+    # get profile indices corresponding to transects used for validation
+    inds_transects = np.isin(data.variables['profileNumber'][:], profiles)
+
+    # read variables of interest
+    if np.sum(inds_transects) > 0:
+        # time
+        t = data.variables['time'][inds_transects]
+        t_dtm = ref_t + timedelta(seconds=np.median(t))
+
+        # survey number
+        survey_number = np.array(data.variables['surveyNumber'][:])[0]
+        # survey data
+        survey_data[str(survey_number)] = dict([])
+        survey_data[str(survey_number)]['date'] = t_dtm
+        survey_data[str(survey_number)]['latitude'] = np.array(data.variables['lat'][:])
+        survey_data[str(survey_number)]['longitude'] = np.array(data.variables['lon'][:])
+        survey_data[str(survey_number)]['x'] = np.array(data.variables['xFRF'][:])
+        survey_data[str(survey_number)]['y'] = np.array(data.variables['yFRF'][:])
+        survey_data[str(survey_number)]['elevation'] = np.array(data.variables['elevation'][:])
+        survey_data[str(survey_number)]['profile'] = np.array(data.variables['profileNumber'][:])
+
+# initialization of output variables
 time = []
 elevation = []
 cross_sh_d = []
 profil = []
 survey_number = []
 
-# read all nc files
-for i, f in enumerate(ls_raw):
-    print(f)
-    data = Dataset(f)
+# loop through surveys
+for n in survey_data.keys():
+    survey = survey_data[n]
+    for i in range(len(pf_names)):
+        idx = np.where(survey['profile'] == profiles[i])[0]
+        if len(idx) < 3:
+            continue
 
-    # get profile indices corresponding to transects used for validation
-    profil_tmp = data.variables['profileNumber'][:]
-    inds_transects = np.isin(profil_tmp, transects)
+        # read csd and elevation
+        csd = survey['x'][idx]
+        el = survey['elevation'][idx]
 
-    if np.sum(inds_transects) > 0:
+        # sort by csd
+        idx_sorted = np.argsort(csd)
+        csd = csd[idx_sorted]
+        el = el[idx_sorted]
 
-        # list transects that have data for the considered date
-        profil_tmp = data.variables['profileNumber'][inds_transects]
-        transects_tmp = sorted(set(profil_tmp))
-
-        # select data at transects
-        t = data.variables['time'][inds_transects]
-        t_dtm = ref_t + timedelta(seconds=np.median(t))
-        for transect in transects_tmp:
-            i_tr = np.where(profil_tmp == transect)[0]
-            time.append(t_dtm)
-            profil.append(transect)
-            csd = data.variables['xFRF'][i_tr]
-            el = data.variables['elevation'][i_tr]
-            isort_csd = np.argsort(csd)
-            cross_sh_d.append(csd[isort_csd])
-            elevation.append(el[isort_csd])
+        # append to output variables
+        cross_sh_d.append(csd)
+        elevation.append(el)
+        profil.append(profiles[i])
+        time.append(survey_data[n]['date'])
 
 # create a dataframe
 df = pd.DataFrame({'date': time, 'profile_id': profil, 'cross_sh_d':cross_sh_d, 'elevation': elevation})
