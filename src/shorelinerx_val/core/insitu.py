90b2720ac7d891d150c35866669967f9d0da357f
Florent <florent.fournier@wavesnsee.com>
2026-09-01 21:23:21 +0200

    create function 'cross_shore_at_elevation'

--- src/shorelinerx_val/core/insitu.py
+++ src/shorelinerx_val/core/insitu.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 
 def read_bp(f_insitu_bp: Path):
@@ -13,3 +14,14 @@ def read_bp(f_insitu_bp: Path):
     df = df[df['elevation'].apply(lambda x: len(x) > 0)].reset_index(drop=True)
 
     return df
+
+
+def cross_shore_at_elevation(x, y, target_elev):
+    '''
+    assess from beach profile the cross shore distance corresponding to a given height
+    '''
+    x = np.asarray(x)
+    y = np.asarray(y)
+    if y[0] > y[-1]:
+        x, y = x[::-1], y[::-1]
+    return np.interp(target_elev, y, x)
