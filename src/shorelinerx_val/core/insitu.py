1a19dd5691f1e3870007df1853893b20dea0848e
Florent <florent.fournier@wavesnsee.com>
2026-09-01 20:32:26 +0200

    remove empty rows of beach profile

--- src/shorelinerx_val/core/insitu.py
+++ src/shorelinerx_val/core/insitu.py
@@ -8,6 +8,8 @@ def read_bp(f_insitu_bp: Path):
     :param f_insitu_bp: geoparquet file of beach profile
     :return: dataframe
     '''
+
     df = pd.read_parquet(f_insitu_bp)
+    df = df[df['elevation'].apply(lambda x: len(x) > 0)].reset_index(drop=True)
 
     return df
