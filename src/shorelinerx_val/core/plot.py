766b7a43769b5589a71abb339ead09fe7d0581bf
Florent <florent.fournier@wavesnsee.com>
2026-09-15 12:11:08 +0200

    make output filename more concise

--- src/shorelinerx_val/core/plot.py
+++ src/shorelinerx_val/core/plot.py
@@ -427,7 +427,7 @@ def make(df_tr: pd.DataFrame, table_tr_id: dict, ls_df_dbw: list[pd.DataFrame], 
     layout = column(radio, *layouts_val, sizing_mode='stretch_width')
 
     # save
-    f_out = odir.joinpath(f'validation_{site}_{mission}_{'_'.join(sdi_id)}.html')
+    f_out = odir.joinpath(f'val_{site}_{mission}_{'_'.join(sdi_id)}.html')
     output_file(f_out)
     print('\n --> %s \n' %f_out)
     save(layout, title='Validation Shorelinerx')
